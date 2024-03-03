bcbfb56b27129348b887b27d52ac150e66afb064
belkacemm <belkacemmezhoud@comcast.net>
2024-03-02 19:14:22 -0600

    added environment video

--- display.py
+++ display.py
@@ -6,3 +6,8 @@ if selected_video == "Overview":
     my_video = open("videos/overview.mkv", "rb")
     my_video_in_bytes = my_video.read()
     st.video(my_video_in_bytes)
+
+if selected_video == "Set Up an Environment":
+    my_video = open("videos/environment.mkv", "rb")
+    my_video_in_bytes = my_video.read()
+    st.video(my_video_in_bytes)    
