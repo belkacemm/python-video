42bd117198e3e30c704b5f555252748ab446338f
belkacemm <belkacemmezhoud@comcast.net>
2024-03-06 17:54:19 -0600

    added data structures: List video

--- display.py
+++ display.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-selected_video = st.selectbox("Select a video you want to watch", ["Select one video", "Overview", "Set Up an Environment"], index=0)
+selected_video = st.selectbox("Select a video you want to watch", ["Select one video", "Overview", 
+                                                                   "Set Up an Environment", 
+                                                                   "Data Structures: List"], index=0)
 
 if selected_video == "Overview":
     my_video = open("videos/overview.mkv", "rb")
@@ -10,4 +12,9 @@ if selected_video == "Overview":
 if selected_video == "Set Up an Environment":
     my_video = open("videos/environment.mkv", "rb")
     my_video_in_bytes = my_video.read()
-    st.video(my_video_in_bytes)    
+    st.video(my_video_in_bytes)
+
+if selected_video == "Data Structures: List":
+    my_video = open("videos/data_structures_list.mkv", "rb")
+    my_video_in_bytes = my_video.read()
+    st.video(my_video_in_bytes)        
